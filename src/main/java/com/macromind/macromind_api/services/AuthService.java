80f755690899c1d05a6a43dc2bf6c6a3888836e6
SaurabhS55 <saurabhsalunke91@gmail.com>
2026-03-15 19:22:48 +0530

    Added signup and login changes

--- src/main/java/com/macromind/macromind_api/services/AuthService.java
+++ src/main/java/com/macromind/macromind_api/services/AuthService.java
@@ -2,11 +2,15 @@ package com.macromind.macromind_api.services;
 
 import com.macromind.macromind_api.dtos.AuthRequest;
 import com.macromind.macromind_api.dtos.AuthResponse;
+import com.macromind.macromind_api.dtos.CommonResponse;
 import com.macromind.macromind_api.dtos.RegisterRequest;
 import com.macromind.macromind_api.models.UserModel;
 import com.macromind.macromind_api.repositories.AuthRepository;
 import com.macromind.macromind_api.utils.JwtUtil;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -14,6 +18,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class AuthService {
 
     private final AuthRepository repository;
@@ -22,31 +27,51 @@ public class AuthService {
     private final AuthenticationManager authenticationManager;
     private final CustomUserDetailsService userDetailsService;
 
-    public AuthResponse register(RegisterRequest request) {
-        var user = UserModel.builder()
-                .name(request.getName())
-                .email(request.getEmail())
-                .password(passwordEncoder.encode(request.getPassword()))
-                .dob(request.getDob())
-                .gender(request.getGender())
-                .build();
-        repository.save(user);
-        var jwtToken = jwtUtil.generateToken(userDetailsService.loadUserByUsername(user.getEmail()));
-        return AuthResponse.builder()
-                .token(jwtToken)
-                .build();
+    public CommonResponse register(RegisterRequest request) {
+        try {     
+                if(repository.findByEmail(request.getEmail()).isPresent()) {
+                    throw new Exception("User already exists");
+                }
+                var user = UserModel.builder()
+                        .name(request.getName())
+                        .email(request.getEmail())
+                        .password(passwordEncoder.encode(request.getPassword()))
+                        .dob(request.getDob())
+                        .gender(request.getGender())
+                        .build();
+                repository.save(user);
+                log.info("User registered successfully: {}", user.getEmail());
+                return CommonResponse.builder()
+                        .status(HttpStatus.OK.toString())
+                        .message("User registered successfully")
+                        .build();
+        } catch (Exception e) {
+                log.error("Error registering user: {}", e.getMessage());
+            return CommonResponse.builder()
+                    .status(HttpStatus.BAD_REQUEST.toString())
+                    .message(e.getMessage())
+                    .build();
+        }
     }
 
     public AuthResponse login(AuthRequest request) {
-        authenticationManager.authenticate(
+        try{
+            authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
                         request.getEmail(),
                         request.getPassword()));
-        var user = repository.findByEmail(request.getEmail())
-                .orElseThrow();
-        var jwtToken = jwtUtil.generateToken(userDetailsService.loadUserByUsername(user.getEmail()));
-        return AuthResponse.builder()
-                .token(jwtToken)
-                .build();
+            UserModel user = repository.findByEmail(request.getEmail())
+                    .orElseThrow();
+            String jwtToken = jwtUtil.generateToken(userDetailsService.loadUserByUsername(user.getEmail()));
+            log.info("User logged in successfully: {}", user.getEmail());
+            return AuthResponse.builder()
+                    .token(jwtToken)
+                    .build();
+        }catch(Exception e){
+                log.error("Error logging in: {}", e.getMessage());
+            return AuthResponse.builder()
+                    .token(null)
+                    .build();
+        }
     }
 }
