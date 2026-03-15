53ec002eee4746b1c80f1bc0fdb72a299042dcb2
SaurabhS55 <saurabhsalunke91@gmail.com>
2026-03-15 19:14:25 +0530

    Added required dto

---
+++ src/main/java/com/macromind/macromind_api/dtos/ErrorResponse.java
@@ -0,0 +1,18 @@
+package com.macromind.macromind_api.dtos;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorResponse {
+    private int status;
+    private String message;
+    private LocalDateTime timestamp;
+}
